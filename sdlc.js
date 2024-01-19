f96032ee46cb4735bf9ff385d3e6758c80df7c20
Viviana Rosenbaum <vivianarosenbaum@gmail.com>
2024-01-18 19:59:06 -0700

    Initial commit

---
+++ sdlc.js
@@ -0,0 +1,26 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "This is the second step where the requirements for the software are gathered, analyzed, and documented. It involves understanding the needs of the end-users and stakeholders.";
+const design = "This is the thrid step, in which the software architecture is planned. It involves creating a blueprint or design document that outlines how the software will be structured and how its components will interact.";
+const implementation = "This is the fourth step where the actual code is written based on the design specifications. It involves translating the design into executable code.";
+const testingAndIntegration = "This is the fifth step, which involves testing the software to ensure that it works as intended. Integration testing is also performed to verify that different components of the software work together seamlessly.";
+const maintain = "This is the final step. After the software is deployed, this step involves ongoing maintenance and support. It includes fixing bugs, making updates, and addressing issues that may arise during regular usage.";
